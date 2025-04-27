fea2d453b90a4bc28cbdce7c127fffba5d91adcb
Rohan Vohra <rohanvohra055@gmail.com>
2025-04-27 20:19:19 +0530

    Fix: cors error fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
